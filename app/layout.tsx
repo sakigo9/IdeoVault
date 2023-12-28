ed3d4b1007e75133e7a1a61baf83c72d67728677
sakigo9 <saketprag322@gmail.com>
2023-12-28 14:16:26 +0530

    feat: added emoji picker, upload image using edgestore

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,6 +5,7 @@ import { ThemeProvider } from '@/components/providers/theme-provider'
 import { ConvexClientProvider } from '@/components/providers/convex.provider'
 import { Toaster } from 'sonner'
 import { ModalProvider } from '@/components/providers/modal-provider'
+import { EdgeStoreProvider } from '../lib/edgestore'
 
 const inter = Poppins({ subsets: ['latin'], weight: ['400', '600'] })
 
@@ -23,17 +24,19 @@ export default function RootLayout({
     <html lang='en' suppressHydrationWarning>
       <body className={inter.className}>
         <ConvexClientProvider>
-          <ThemeProvider
-            attribute='class'
-            defaultTheme='system'
-            enableSystem
-            disableTransitionOnChange
-            storageKey='ideo-vault-theme'
-          >
-            <Toaster position='bottom-right' />
-            <ModalProvider />
-            {children}
-          </ThemeProvider>
+          <EdgeStoreProvider>
+            <ThemeProvider
+              attribute='class'
+              defaultTheme='system'
+              enableSystem
+              disableTransitionOnChange
+              storageKey='ideo-vault-theme'
+            >
+              <Toaster position='bottom-right' />
+              <ModalProvider />
+              {children}
+            </ThemeProvider>
+          </EdgeStoreProvider>
         </ConvexClientProvider>
       </body>
     </html>
